68f3d6f37b415c801bfa45159739d3dbe1ee6a5c
iamdavidxu <davidxbsxbsxbs@gmail.com>
2024-09-22 00:23:58 -0700

    done

--- my_web/src/components/Playhouse.js
+++ my_web/src/components/Playhouse.js
@@ -1,12 +1,20 @@
 import React from 'react';
+let audioContext = null;
 
+document.addEventListener('click', () => {
+	if (!audioContext) {
+		audioContext = new (window.AudioContext || window.webkitAudioContext)();
+	} else if (audioContext.state === 'suspended') {
+		audioContext.resume();
+	}
+});
 function Playhouse() {
 	return (
 		<div className='playhouse'>
 			<h3>Playhouse</h3>
 			<div className='playContainer'>
 				<iframe
-					src='https://my.spline.design/untitled-dbe6ca7e88ecb5951de6a619e05aa4da/'
+					src='https://my.spline.design/untitled-dbe6ca7e88ecb5951de6a619e05aa4da/?v=latest'
 					width='100%'
 					height='100%'
 				></iframe>
